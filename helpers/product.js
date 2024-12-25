963f3c2ba9896fe3c35173fc884427176a271c4d
unknown <phamlong123np@gmail.com>
2024-12-25 22:17:09 +0700

    Cập nhật giá giao diện

--- helpers/product.js
+++ helpers/product.js
@@ -1,11 +1,13 @@
 module.exports.priceNewproduct=(products)=>{
     const newProduct=products.map(item=>{
-        item.priceNew=(item.price-(item.price*(item.discountPercentage/100))).toFixed(0)
+        // toFixed(0): Làm tròn số đến 0 chữ số thập phân kết quả trả vèe là 1 chuỗi
+        item.priceNew=parseInt((item.price-(item.price*(item.discountPercentage/100))).toFixed(0))
         return item
     })
     return newProduct
 }
+
 module.exports.priceNew=(product)=>{
-    const priceNew=(product.price-(product.price*(product.discountPercentage/100))).toFixed(0)
+    const priceNew=parseInt((product.price-(product.price*(product.discountPercentage/100))).toFixed(0))
     return priceNew
 }
