9802e1fe4cae48e13842e84dc390c696696fca99
unknown <phamlong123np@gmail.com>
2024-12-25 23:09:22 +0700

    Cập nhật dashboard

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,7 @@
 const ProductCategory=require("../../model/products-category.model")
 const Product=require('../../model/product.model')
-const Account=require('../../model/account.model')
+const Account=require('../../model/account.model');
+const User = require("../../model/user.model");
 module.exports.index=async(req,res)=>{
     const statistic = {
         categoryProduct: {
@@ -29,11 +30,16 @@ module.exports.index=async(req,res)=>{
     statistic.categoryProduct.inactive=await ProductCategory.countDocuments({deleted:false,status:"inactive"})//khi truy
     
     statistic.product.total=await Product.countDocuments({deleted:false})//khi truyền {}
-    statistic.product.active=await Product.countDocuments({deleted:false,status:"active"})//khi truy
-    statistic.product.inactive=await Product.countDocuments({deleted:false,status:"inactive"})//khi truy
+    statistic.product.active=await Product.countDocuments({deleted:false,status:"active"})
+    statistic.product.inactive=await Product.countDocuments({deleted:false,status:"inactive"})
 
-    statistic.account.total=await Account.countDocuments({deleted:false})//khi truyền {}
-    statistic.account.active=await Account.countDocuments({deleted:false,status:"active"})//khi truy
-    statistic.account.inactive=await Account.countDocuments({deleted:false,status:"inactive"})//khi truy
+    statistic.account.total=await Account.countDocuments({deleted:false})
+    statistic.account.active=await Account.countDocuments({deleted:false,status:"active"})
+    statistic.account.inactive=await Account.countDocuments({deleted:false,status:"inactive"})
+
+
+    statistic.user.total=await User.countDocuments({deleted:false})
+    statistic.user.active=await User.countDocuments({deleted:false,status:"active"})
+    statistic.user.inactive=await User.countDocuments({deleted:false,status:"inactive"})
     res.render('admin/pages/dashboard/index',{title:"Trang tổng quan",statistic:statistic})
 }
