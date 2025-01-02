1a1aedf7fff6a8a1cfc117cbcb5225705023ca12
unknown <phamlong123np@gmail.com>
2025-01-02 17:16:18 +0700

    Phân trang cho bài viết

--- controllers/client/article.controller.js
+++ controllers/client/article.controller.js
@@ -1,19 +1,27 @@
 const Article = require("../../model/article.model")
-
+const paginationHelper=require("../../helpers/pagination");
 module.exports.index=async(req,res)=>{
     const articlesFeatured =await Article.find({
         deleted: false,
         featured:"1"
-    }).select('title shortDescription thumbnail slug createdBy')
+    }).select('title shortDescription thumbnail slug createdBy').limit(6).select('title shortDescription thumbnail slug createdBy')
+    
+    const countArtists=await Article.countDocuments({deleted: false,})
+    const objectPagination=paginationHelper(req.query,countArtists,{
+        currentPage:1,
+        limitItems:4
+    }) 
     const articles =await Article.find({
         deleted: false,
     }).sort({
        'createdBy.createdAt': 'desc'
-    }).limit(6).select('title shortDescription thumbnail slug createdBy')
+    }).limit(objectPagination.limitItems).skip(objectPagination.skip).select('title shortDescription thumbnail slug createdBy')
+    
     res.render('client/pages/articles/index',{
         title:'Tạp chí thức ăn',
         articlesFeatured:articlesFeatured,
-        articles:articles
+        articles:articles,
+        pagination:objectPagination,
     })
 }
 module.exports.detail=async(req,res)=>{
