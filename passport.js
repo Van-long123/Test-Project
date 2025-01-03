8f675fc441b7ae8e72ec8bc2021989f91d6d517d
unknown <phamlong123np@gmail.com>
2025-01-03 16:10:31 +0700

    Đăng nhập qua google

--- passport.js
+++ passport.js
@@ -37,14 +37,3 @@ passport.use(new GoogleStrategy({
 ));
 
 
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
