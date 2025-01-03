a1664e6012ed4487c763c4db006394c5b3e578df
unknown <phamlong123np@gmail.com>
2025-01-03 16:31:06 +0700

    Đăng nhập qua google

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // nơi Google chuyển hướng sau khi xác thực thành công
-    callbackURL: "https://food-project-nu-opal.vercel.app/auth/google/callback"
+    callbackURL: "/auth/google/callback"
   },
     async (accessToken, refreshToken, profile, cb) =>{
         // thêm user ở đây 
